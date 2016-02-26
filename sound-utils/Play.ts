5d8ecd3f97210c44224da51ba69bc1ad203436aa
cody <cevek@ya.ru>
2016-02-26 12:16:18 +0500

    Added play.state

--- sound-utils/Play.ts
+++ sound-utils/Play.ts
@@ -1,12 +1,15 @@
+export const enum PlayingStatus {
+    PLAYING, STOPPING
+}
 export class Play {
     private playSource:AudioBufferSourceNode;
     private startPlayTime = 0;
     private start = 0;
     private dur = 0;
     private audioBuffer:AudioBuffer;
+    private state = PlayingStatus.STOPPING;
 
-    constructor(public audioContext:AudioContext) {
-    }
+    constructor(public audioContext:AudioContext) {}
 
     setAudio(audioBuffer:AudioBuffer) {
         this.audioBuffer = audioBuffer;
@@ -26,28 +29,41 @@ export class Play {
         return buff;
     }
 
-    play(start:number, dur:number, loop = false) {
+    play(start:number, dur:number, loop = false, onEnded?: ()=>void) {
+        this.state = PlayingStatus.PLAYING;
         this.stop();
         this.playSource = this.audioContext.createBufferSource();
-        this.playSource.buffer = this.audioBuffer;
+        this.playSource.buffer = this.cutAudioBuffer(start, start + dur);
         this.playSource.connect(this.audioContext.destination);
         this.playSource.onended = ()=>this.stop();
-
         if (!this.audioBuffer) {
             throw new Error('audioBuffer is empty');
         }
         this.start = start;
         this.dur = dur;
         this.startPlayTime = this.audioContext.currentTime;
-        this.playSource.start(0, start, dur);
+        this.playSource.start(0);
         this.playSource.loop = loop;
+        this.playSource.onended = this.onEnded;
+        if (onEnded) {
+            this.playSource.addEventListener('ended', onEnded);
+        }
+    }
+
+    private onEnded = () => {
+        this.state = PlayingStatus.STOPPING;
     }
 
     stop() {
         if (this.playSource) {
             this.playSource.stop();
+            this.playSource.onended = null;
         }
         this.playSource = null;
+    }
+
+    getState() {
+        return this.state;
     }
 
     getCurrentTime() {
